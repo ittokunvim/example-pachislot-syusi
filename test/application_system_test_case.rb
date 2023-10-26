6c7d770fdb15bca364102428ad7c935965db10da
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2023-10-27 01:47:00 +0900

    feat: :hammer: Add Selenium for the test
    Issue: #8

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,10 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: {
+    browser: :remote,
+    url: "http://selenium:4444",
+  }
+
+  Capybara.server_host = ENV.fetch("HOSTNAME")
 end
