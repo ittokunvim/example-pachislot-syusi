0f5b0a41dfe7312d5539ea4ef482ab85f977ef6f
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2023-11-13 03:49:12 +0900

    feat: :white_check_mark: Add header, footer test
    Issue: #23

---
+++ test/system/layouts_test.rb
@@ -0,0 +1,19 @@
+require "application_system_test_case"
+
+class LayoutsTest < ApplicationSystemTestCase
+  test "visiting the root" do
+    visit root_url
+
+    # header
+    assert_link "", href: root_path
+    assert_text I18n.t("layouts.header.blance")
+    assert_link I18n.t("layouts.header.index"), href: blances_path
+    assert_link I18n.t("layouts.header.new"), href: new_blance_path
+    # footer
+    assert_link "Ruby on Rails", href: "https://rubyonrails.org/"
+    assert_link "render", href: "https://render.com/"
+    assert_link "PostgreSQL", href: "https://www.postgresql.org/"
+    assert_link "GitHub", href: "https://github.com"
+    assert_text "ittokunvim All Rights Reserved."
+  end
+end
