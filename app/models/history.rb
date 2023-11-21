4ab1bd070469187f4c1dad3494cd0c23ef1b1c73
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2023-11-21 19:19:25 +0900

    feat: :heavy_plus_sign: Add History model
    Issue: #35

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,2 +1,6 @@
 class History < ApplicationRecord
+  validates :game_count, presence: true, numericality: { less_than: 2**31 }
+  validates :chance, presence: true, length: { maximum: 100 }
+  validates :investment, presence: true, length: { maximum: 100 }
+  validates :memo, length: { maximum: 1000 }
 end
