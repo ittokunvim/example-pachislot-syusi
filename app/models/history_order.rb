0122578316b5f205958dbcec07d6a13aadb11d86
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2023-11-22 19:40:02 +0900

    feat: :heavy_plus_sign: Add HistoryOrder model
    Issue: #35

--- app/models/history_order.rb
+++ app/models/history_order.rb
@@ -1,2 +1,4 @@
 class HistoryOrder < ApplicationRecord
+  ORDER_REGEX = /(\d+,)+\d+\Z/
+  validates :order, presence: true, length: { maximum: 1000 }, format: { with: ORDER_REGEX }
 end
--- test/models/history_order_test.rb
+++ test/models/history_order_test.rb
@@ -1,7 +1,46 @@
 require "test_helper"
 
 class HistoryOrderTest < ActiveSupport::TestCase
-  test "should be true" do
-    assert true
+  def setup
+    @history_order = history_orders(:one)
+  end
+
+  test "should be valid" do
+    assert @history_order.valid?
+  end
+
+  test "should be invalid" do
+    assert HistoryOrder.new.invalid?
+  end
+
+  test "order should be present" do
+    @history_order.order = "     "
+    assert_not @history_order.valid?
+  end
+
+  test "order should not be too long" do
+    @history_order.order = "a" * 1001
+    assert_not @history_order.valid?
+  end
+
+  test "order should be valid format" do
+    valid_orders = %w[1,2,3 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+    valid_orders.each do |valid_order|
+      @history_order.order = valid_order
+      assert @history_order.valid?, "#{valid_order.inspect} should be valid"
+    end
+  end
+
+  test "order should be invalid format" do
+    invalid_orders = %w[
+      hogebar
+      1, 2, 3, 4, 5
+      6,7,8,9,10,11,
+      12,13, 14,15, 16
+    ]
+    invalid_orders.each do |invalid_order|
+      @history_order.order = invalid_order
+      assert @history_order.invalid?, "#{invalid_order.inspect} should be invalid"
+    end
   end
 end
