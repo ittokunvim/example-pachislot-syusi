b0195d2554dbc16d6c1e3f424fe582d835f84762
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2023-11-28 03:06:07 +0900

    feat: :memo: Add HTML for histories edit form
    Issue: #43

--- test/system/histories_test.rb
+++ test/system/histories_test.rb
@@ -7,8 +7,11 @@ class HistoriesTest < ApplicationSystemTestCase
   end
 
   test "visiting the index" do
+    # no history
+    visit blance_index_history_url(blances(:most_recent))
+    assert_text I18n.t("histories.index.no_history")
+    # has history
     visit blance_index_history_url(@blance)
-
     excepted_columns = %w[id created_at updated_at blance_id]
     @histories.each do |history|
       history.attributes.each do |k, v|
@@ -38,6 +41,23 @@ class HistoriesTest < ApplicationSystemTestCase
     click_on I18n.t("histories.index.button_text")
     # check redirect
     assert_selector "div#flash_notice", text: I18n.t("histories.create.notice")
+    assert_text @history.memo
+  end
+
+  test "updating a History" do
+    @history = @histories.first
+    @history.memo = "testtest"
+    visit edit_blance_history_url(@blance, @history)
+
+    # invalid input
+    fill_in I18n.t("activerecord.attributes.history.game_count"), with: ""
+    click_on I18n.t("histories.edit.button_text")
+    assert_selector "div#flash_alert"
+    # valid input
+    fill_in I18n.t("activerecord.attributes.history.memo"), with: @history.memo
+    click_on I18n.t("histories.edit.button_text")
+    # check redirect
+    assert_selector "div#flash_notice", text: I18n.t("histories.update.notice")
     assert_text @history.memo
   end
 end
