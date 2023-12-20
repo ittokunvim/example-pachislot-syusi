4e61c500cb27d869b5bfe669f4d3aaf59e4c08c6
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2023-12-21 02:15:30 +0900

    fix: :truck: Moved js code from js controller to blances HTML

--- app/javascript/controllers/blances_controller.js
+++ app/javascript/controllers/blances_controller.js
@@ -2,21 +2,4 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="blances"
 export default class extends Controller {
-  static targets = ["result"];
-
-  connect() {
-    const result = this.resultTarget;
-    const resultTextLength = result.textContent.replaceAll(/\+/g, "").length;
-    const resultText = result.textContent.replaceAll(/,|円/g, "");
-    const resultNum = Number(resultText);
-
-    if (resultNum === 0) {
-      this.resultTarget.classList.add("zero");
-    } else if (resultNum > 0) {
-      result.textContent = result.textContent.padStart(resultTextLength + 1, "+");
-      this.resultTarget.classList.add("plus");
-    } else {
-      this.resultTarget.classList.add("minus");
-    }
-  }
 }
